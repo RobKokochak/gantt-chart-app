b9b5b95ed53be1c853768d8cbc187592c9acaebd
Greg Kosakowski <99287183+gkosakow@users.noreply.github.com>
2022-11-30 22:53:53 -0500

    exporting task data to gantt chart

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,10 +1,19 @@
 import Task from './Task';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemText from '@mui/material/ListItemText';
+import { FixedSizeList } from 'react-window';
+import { Paper } from '@mui/material';
 
 const TaskList = ({ tasks, removeTask }) => {
+	console.log(tasks);
+	//sort the array
 	return (
-		<div className="container">
-			<ul>{tasks.map((task) => <Task key={task.id} task={task} removeTask={removeTask} />)}</ul>
-		</div>
+		<Paper style={{ maxHeight: 470, overflow: 'auto', borderRadius: 25}}>
+			<List className="inner-container" sx={{ width: '100%', bgcolor: 'rgba(92, 103, 140, 0.4)' }}>
+				<ul>{tasks.map((task) => <Task key={task.id} task={task} removeTask={removeTask} />)}</ul>
+			</List>
+		</Paper>
 	);
 }
 
